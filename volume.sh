f7d89633f0910298733579a70f7c41511e4c6d67
dimsp98 <dspiliotis98@gmail.com>
2024-03-27 12:38:45 +0200

    updated

--- volume.sh
+++ volume.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-vol=$(pamixer --get-volume)
-volStatus=$(pamixer --get-volume-human)
+#vol=$(pamixer --get-volume)
+#volStatus=$(pamixer --get-volume-human)
+vol=$(pulsemixer --get-volume)
+volStatus=$(pulsemixer --get-volume-human)
 
 printf "%s %s%%" "$icon" "$vol"
