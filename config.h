9c5a5359cd8dc5cfbfa2cd989cbc8085e08f0a6f
dimsp98 <dspiliotis98@gmail.com>
2024-03-27 12:36:08 +0200

    updated

--- config.h
+++ config.h
@@ -69,14 +69,14 @@ static const struct arg args[] = {
     {keymap, " %s ", NULL},
     /*   {netspeed_tx, "^c#89b4fa^^b#1e1e2e^ ^c#cdd6f4^^b#1e1e2e^%sB/s ^d^",
        "enp3s0"}, */
-    {run_command, "^c#74c7ec^^b#1e1e2e^^c#cdd6f4^^b#1e1e2e^%s ^d^",
+   /* {run_command, "^c#74c7ec^^b#1e1e2e^^c#cdd6f4^^b#1e1e2e^%s ^d^",
      "~/slstatus/volume.sh"},
     /*
         {ram_perc, "^c#f9e2af^^b#1e1e2e^ ^c#cdd6f4^^b#1e1e2e^%s%% ^d^",
        NULL}, {cpu_perc, "^c#a6e3a1^^b#1e1e2e^ ^c#cdd6f4^^b#1e1e2e^%s%% ^d^",
        NULL},
-
     */
+    {vol_perc, "^c#74c7ec^^b#1e1e2e^^c#cdd6f4^^b#1e1e2e^%s^d^", NULL},
     {datetime, "^c#f38ba8^^b#1e1e2e^ ^c#cdd6f4^^b#1e1e2e^%s ^d^",
      "%a %d %B ^c#f38ba8^^b#1e1e2e^ ^c#cdd6f4^^b#1e1e2e^%H:%M   "},
 };
